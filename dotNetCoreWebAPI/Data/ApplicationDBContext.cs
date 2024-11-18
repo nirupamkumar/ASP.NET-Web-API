11b0987a75bf12c5380b45c9c97a1508f85efd47
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-18 22:48:02 +0100

    Configure ApplicationDBContext for database context

---
+++ dotNetCoreWebAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using dotNetCoreWebAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNetCoreWebAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
