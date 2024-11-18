547a055bae6ac744dd86d62fb9b4b8ef2bfa0e04
nirupamkumar <mundra.nirupamkumar@gmail.com>
2024-11-18 22:46:54 +0100

    Add Employee entity class

---
+++ dotNetCoreWebAPI/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace dotNetCoreWebAPI.Models.Entities
+{
+    public class Employee
+    {
+        public int GuId { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone {  get; set; }
+        public decimal Salary { get; set; }
+    }
+}
